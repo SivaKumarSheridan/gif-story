bb3115c867336afd90b1c17dcb8d8ce53e8bde91
Mimi Xuan <moimimixuan@gmail.com>
2022-04-05 14:32:07 -0400

    added react-icon npm

--- src/app/modules/pages/Dashboard.js
+++ src/app/modules/pages/Dashboard.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { FaHatCowboy } from "react-icons/fa";
 
 export default function Dashboard() {
-
-    return(<>
-    <div>Hello From Dashboard</div>
-    </>);
+  return (
+    <>
+      <div>
+        Hello From Dashboard <FaHatCowboy />
+      </div>
+    </>
+  );
 }
