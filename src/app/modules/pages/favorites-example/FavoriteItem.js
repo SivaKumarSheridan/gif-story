33aaa4db3209d9c81401812a7a8c83b9513d44e9
Mimi Xuan <moimimixuan@gmail.com>
2022-04-05 14:47:47 -0400

    favlist add gifs, install sass

--- src/app/modules/pages/favorites-example/FavoriteItem.js
+++ src/app/modules/pages/favorites-example/FavoriteItem.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-export default function FavoriteItem({data}){
-
-    return(
-        <>
-        <h2>{data.id}</h2>
-        <h2>{data.name}</h2>
-        </>
-    );
+export default function FavoriteItem({ data }) {
+  return (
+    <>
+      <h2>{data.id}</h2>
+      <h2>{data.createdDate}</h2>
+      <h2>{data.quote}</h2>
+      <img src={data.img1} alt="" />
+      <img src={data.img2} alt="" />
+      <img src={data.img3} alt="" />
+      <h2>{data.isFav}</h2>
+    </>
+  );
 }
--- src/app/modules/pages/favorites-example/FavoritesList.js
+++ src/app/modules/pages/favorites-example/FavoritesList.js
@@ -1,33 +1,44 @@
 import React from "react";
 import FavoriteItem from "./FavoriteItem";
 
-export default function FavoritesList(){
-
-const arrayList =[
+export default function FavoritesList() {
+  const arrayList = [
     {
-        id:1,
-        name:"test1"
+      id: 1,
+      createdDate: "04-05-2022",
+      quote: "Never Gonna Give You Up",
+      img1: "https://media.giphy.com/media/aECVcAuxVPluABf0d0/giphy.gif",
+      img2: "https://media.giphy.com/media/ZpO6lCz6tbSCXn6nXq/giphy.gif",
+      img3: "https://media.giphy.com/media/9gIJLP6QZFbVzPmLU9/giphy.gif",
+      isFav: true,
     },
     {
-        id:2,
-        name:"test2"
+      id: 2,
+      createdDate: "04-04-2022",
+      quote: "Since You've Been Gone",
+      img1: "https://media.giphy.com/media/aECVcAuxVPluABf0d0/giphy.gif",
+      img2: "https://media.giphy.com/media/ZpO6lCz6tbSCXn6nXq/giphy.gif",
+      img3: "https://media.giphy.com/media/9gIJLP6QZFbVzPmLU9/giphy.gif",
+      isFav: true,
     },
     {
-        id:3,
-        name:"test3"
-    }
-]
+      id: 3,
+      createdDate: "04-02-2022",
+      quote: "Do You Believe in Love After Love",
+      img1: "https://media.giphy.com/media/aECVcAuxVPluABf0d0/giphy.gif",
+      img2: "https://media.giphy.com/media/ZpO6lCz6tbSCXn6nXq/giphy.gif",
+      img3: "https://media.giphy.com/media/9gIJLP6QZFbVzPmLU9/giphy.gif",
+      isFav: false,
+    },
+  ];
 
-    return(
-        <>
-        
-<br></br>
-<h1>Favorites</h1>
-{   
-    arrayList.map((item)=>{
-       return (<FavoriteItem data={item} />);
-    })
-}
-        </>
-    );
+  return (
+    <>
+      <br></br>
+      <h1>Favorites</h1>
+      {arrayList.map((item) => {
+        return <FavoriteItem data={item} />;
+      })}
+    </>
+  );
 }
